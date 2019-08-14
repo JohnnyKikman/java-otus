c2c3ee2b15a682b00f4ca03b9f598e7d82079685
JohnnyKikman <ivan.kozin.96@gmail.com>
2019-08-14 18:26:30 +0300

    Refactor JsonWriterImpl to utilize javax.json package utilities

--- hw07-serialization/src/main/java/ru/otus/writer/JsonWriterImpl.java
+++ hw07-serialization/src/main/java/ru/otus/writer/JsonWriterImpl.java
@@ -2,26 +2,23 @@ package ru.otus.writer;
 
 import lombok.SneakyThrows;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonValue;
 import java.io.Writer;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import static java.lang.String.format;
-import static java.util.stream.Collectors.joining;
-
 public class JsonWriterImpl implements JsonWriter {
-
-    private static final String NULL = "null";
-    private static final String DELIMITER = ",";
-    private static final String STRING_FORMATTING = "\"%s\"";
-    private static final String ARRAY_FORMATTING = "[%s]";
-    private static final String FIELD_FORMATTING = "\"%s\":%s";
 
     private static final Set<Class> VALUE_CLASSES = Set.of(String.class, Enum.class, Number.class, Boolean.class,
             Character.class, Iterable.class, Map.class);
@@ -32,16 +29,14 @@ public class JsonWriterImpl implements JsonWriter {
     @Override
     public String toJson(Object object) {
         if (object == null) {
-            return NULL;
+            return JsonValue.NULL.toString();
         }
 
         if (isValue(object)) {
-            return formatField(object);
+            return formatField(object).toString();
         }
 
-        final Map<String, String> mapOfFields = new LinkedHashMap<>();
-        extractFields(object.getClass(), object, mapOfFields);
-        return formatObject(mapOfFields);
+        return toJsonObject(object).toString();
     }
 
     /**
@@ -55,10 +50,22 @@ public class JsonWriterImpl implements JsonWriter {
     }
 
     /**
+     * Преобразует объект Java в {@link JsonObject}, готовый к сериализации.
+     */
+    private JsonObject toJsonObject(Object object) {
+        final Map<String, JsonValue> mapOfFields = new LinkedHashMap<>();
+        extractFields(object.getClass(), object, mapOfFields);
+
+        final JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+        mapOfFields.forEach(jsonBuilder::add);
+        return jsonBuilder.build();
+    }
+
+    /**
      * Извлекает значения полей из объекта (сначала - объявленные в классе, которому принадлежит сущность; затем, по
      * порядку - поля суперклассов).
      */
-    private void extractFields(Class<?> clazz, Object object, Map<String, String> mapOfFields) {
+    private void extractFields(Class<?> clazz, Object object, Map<String, JsonValue> mapOfFields) {
         Arrays.stream(clazz.getDeclaredFields()).forEach(field -> extractField(field, object, mapOfFields));
         final Class<?> superclass = clazz.getSuperclass();
         if (superclass != null) {
@@ -71,7 +78,7 @@ public class JsonWriterImpl implements JsonWriter {
      * последующей сериализации.
      */
     @SneakyThrows
-    private void extractField(Field field, Object object, Map<String, String> mapOfFields) {
+    private void extractField(Field field, Object object, Map<String, JsonValue> mapOfFields) {
         field.setAccessible(true);
         final Object value = field.get(object);
         if (value != null) {
@@ -83,52 +90,49 @@ public class JsonWriterImpl implements JsonWriter {
      * Форматирует объект в значение формата JSON, в зависимости от типа.
      */
     @SuppressWarnings("unchecked")
-    private String formatField(Object value) {
-        if (value instanceof String || value instanceof Enum || value instanceof Character) {
-            return format(STRING_FORMATTING, value);
-        } else if (value instanceof Number || value instanceof Boolean) {
-            return value.toString();
+    private JsonValue formatField(Object value) {
+        if (value instanceof String || value instanceof Enum) {
+            return Json.createValue(value.toString());
+        } else if (value instanceof Character) {
+            return Json.createValue(String.valueOf((char) value));
+        } else if (value instanceof Boolean) {
+            return (boolean) value ? JsonValue.TRUE : JsonValue.FALSE;
+        } else if (value instanceof Integer) {
+            return Json.createValue((Integer) value);
+        } else if (value instanceof Byte) {
+            return Json.createValue((Byte) value);
+        } else if (value instanceof Short) {
+            return Json.createValue((Short) value);
+        } else if (value instanceof Long) {
+            return Json.createValue((Long) value);
+        } else if (value instanceof Double) {
+            return Json.createValue((Double) value);
+        } else if (value instanceof Float) {
+            return Json.createValue((Float) value);
+        } else if (value instanceof BigDecimal) {
+            return Json.createValue((BigDecimal) value);
+        } else if (value instanceof BigInteger) {
+            return Json.createValue((BigInteger) value);
+        } else if (value.getClass().isArray()) {
+            final int length = Array.getLength(value);
+            final JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+            for (int i = 0; i < length; i++) {
+                arrayBuilder.add(formatField(Array.get(value, i)));
+            }
+            return arrayBuilder.build();
         } else if (value instanceof Iterable) {
             final Iterable iterable = (Iterable) value;
-            return format(ARRAY_FORMATTING, StreamSupport.stream(iterable.spliterator(), false)
-                    .map(this::formatField)
-                    .collect(joining(DELIMITER)));
-        } else if (value.getClass().isArray()) {
-            if (value instanceof Object[]) {
-                final Object[] array = (Object[]) value;
-                return format(ARRAY_FORMATTING, Arrays.stream(array)
-                        .map(this::formatField)
-                        .collect(joining(DELIMITER)));
-            } else {
-                int length = Array.getLength(value);
-                final Object[] result = new Object[length];
-                for (int i = 0; i < length; i++) {
-                    result[i] = Array.get(value, i);
-                }
-                return Arrays.toString(result)
-                        // spaces aren't present in GSON-formatted arrays
-                        .replaceAll(" ", "");
-            }
+            final JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+            StreamSupport.stream(iterable.spliterator(), false)
+                    .forEach(element -> arrayBuilder.add(formatField(element)));
+            return arrayBuilder.build();
         } else if (value instanceof Map) {
-            final Map<String, String> formattedMap = ((Map<?, ?>) value).entrySet().stream()
-                    .collect(Collectors.toMap(
-                            entry -> entry.getKey().toString(),
-                            entry -> formatField(entry.getValue())
-                    ));
-            return formatObject(formattedMap);
+            final JsonObjectBuilder mapBuilder = Json.createObjectBuilder();
+            ((Map) value).forEach((key, mapValue) -> mapBuilder.add(key.toString(), formatField(mapValue)));
+            return mapBuilder.build();
         } else {
-            return toJson(value);
+            return Json.createObjectBuilder(toJsonObject(value)).build();
         }
-    }
-
-    /**
-     * Преобразует объект (в виде {@link Map} полей и уже отформатированных значений) в строку формата JSON
-     */
-    private String formatObject(Map<String, String> mapOfFields) {
-        return "{" + mapOfFields.entrySet().stream()
-                .map(entry -> format(FIELD_FORMATTING, entry.getKey(), entry.getValue()))
-                .collect(joining(DELIMITER))
-                + "}";
     }
 
     /**
